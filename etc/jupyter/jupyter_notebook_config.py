a9ae83f686966b640a317166992144f3d2fa87d4
mazele <etienne.f.mazel@gmail.com>
2020-06-11 09:16:10 +0200

    notary shared data directory

--- etc/jupyter/jupyter_notebook_config.py
+++ etc/jupyter/jupyter_notebook_config.py
@@ -777,7 +777,7 @@
 ## The sqlite file in which to store notebook signatures. By default, this will
 #  be in your Jupyter data directory. You can set it to ':memory:' to disable
 #  sqlite writing to the filesystem.
-c.NotebookNotary.db_file = ':memory:'
+c.NotebookNotary.data_dir = './'
 
 ## The secret key with which notebooks are signed.
 #c.NotebookNotary.secret = b''
